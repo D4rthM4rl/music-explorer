28fbbe3c6df56a5cf3e8f6a91ce2b1ac7e7978b2
Marley Byers <m4rl10@uw.edu>
2023-08-02 16:18:58 -0700

    Reverted

--- src/test/react/src/index.tsx
+++ src/test/react/src/index.tsx
@@ -2,5 +2,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import App2 from './App2'
+const express = require('express')
+const dotenv = require('dotenv');
+
 
 ReactDOM.render(<App2/>, document.getElementById('root'));
