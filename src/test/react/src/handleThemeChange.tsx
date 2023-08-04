b4049124c384b864a5e9469c6d43f5fa56a5edae
Marley Byers <m4rl10@uw.edu>
2023-08-04 04:00:26 -0700

    Made neon theme

--- src/test/react/src/handleThemeChange.tsx
+++ src/test/react/src/handleThemeChange.tsx
@@ -3,7 +3,7 @@ import React, {Component, ReactComponentElement} from 'react';
 export function handleThemeChange (theme: string) {
     // Update the theme based on the selected option value
     const elements = document.querySelectorAll('.themed');
-    const classesToRemove = ['default', 'dark', 'pastel', 'gay','kevin', 'drac', 'marley'];
+    const classesToRemove = ['default', 'dark', 'neon', 'pastel', 'gay','kevin', 'drac', 'barbie', 'marley'];
     elements.forEach((element) =>{
         element.classList.remove(...classesToRemove);
         element.classList.add(theme);
